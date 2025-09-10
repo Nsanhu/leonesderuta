c81d0315313188a3702822e32ccac4b62d8ea732
Nsanhu <nsanhuezalara@gmail.com>
2025-09-09 22:48:26 -0300

    se realiza actualizacióndel page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +13,19 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private router: Router) {
     // Idiomas soportados
     translate.addLangs(['es', 'pt', 'en']);
-
-    // Idioma por defecto
     translate.setDefaultLang('es');
 
-    // Detecta el idioma del navegador
     const browserLang = translate.getBrowserLang();
     translate.use(browserLang?.match(/es|pt|en/) ? browserLang : 'es');
+
+    // Scroll al top en cada cambio de ruta
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo({ top: 0, behavior: 'smooth' }); // o 'auto' si no quieres animación
+    });
   }
 }
